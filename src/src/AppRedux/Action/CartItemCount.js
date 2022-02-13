334e264e73f554179bf90131fac91a27d99ec9e2
ashish bansal <ashishbansal.97@gmail.com>
2022-02-13 22:11:29 +0530

    latest

--- src/src/AppRedux/Action/CartItemCount.js
+++
@@ -1,8 +0,0 @@
-import { getCartCount } from "../../Util";
-
-const cartItemCount = (data) => ({ type: "CART_ITEM_COUNT", payload: data });
-
-export const cartItemCountHandle = () => async (dispatch) => {
-  const length = await getCartCount();
-  dispatch(cartItemCount(length || 0));
-};
--- src/src/AppRedux/Reducer/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import CategoryReducer from "./CategoryReducer";
-import CartItemCount from "./CartItemCount";
-
-export default combineReducers({ CategoryReducer, CartItemCount });
