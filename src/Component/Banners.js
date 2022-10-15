7105499a7a6577f3b2d96b70daed48d7ff98ac88
dharmik patel <codewithenvision@gmail.com>
2022-10-15 13:22:33 +0530

    new changes

--- src/Component/Banners.js
+++ src/Component/Banners.js
@@ -37,7 +37,7 @@ class Banners extends Component {
                   key={index + 1}
                   onClick={() =>
                     window.open(
-                      res.banner_url.replace("badhat.in", "badhat.app")
+                      `https://${res.banner_url}`
                     )
                   }
                 >
